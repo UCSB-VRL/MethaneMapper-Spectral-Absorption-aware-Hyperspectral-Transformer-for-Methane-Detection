2dbf770f3bbeb14769b1259c0ab2de2326848c47
Jiahe <jiahelu2007@gmail.com>
2023-07-30 11:59:24 -0700

    Add Windows Batch files

---
+++ methanemapper/plume_mask_run.bat
@@ -0,0 +1,1 @@
+python hyper_main.py --masks --hyper_path ./data --dataset_file hyper_seg --output_dir ./exps/segm_model --batch_size 12 --num_workers 8 --frozen_weights ./exps/box_model/checkpoint.pth --epoch 100 --lr_drop 15 --use_wandb True
